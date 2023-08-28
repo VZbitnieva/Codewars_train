23e54520232e6033ba06717367a966747c9c53cd
VZbitnieva <v.zbitnieva@gmail.com>
2023-08-28 10:07:48 +0300

    even-or_odd

--- ruby/str_to_float.rb
+++ ruby/str_to_float.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Oh no!
 # Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
 
@@ -11,5 +13,5 @@
 def to_number_array(string_array)
   string_array.map(&:to_f)
 end
-string_array = ["1", "2", "3"]
+string_array = %w[1 2 3]
 puts to_number_array(string_array)
